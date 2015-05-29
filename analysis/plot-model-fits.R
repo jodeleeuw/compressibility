aeb4afacfa397a1715cbb9f9224ece0945fe5206
Josh de Leeuw <josh.deleeuw@gmail.com>
2015-05-29 15:45:02 -0400

    adjust plot and fix required libs

--- analysis/plot-model-fits.R
+++ analysis/plot-model-fits.R
@@ -1,3 +1,6 @@
+source('analysis/DBDA2E-utilities.R')
+require(plyr)
+
 #### load model and experiment data ####
 
 load('data/output/model_run_data.Rdata')
@@ -54,7 +57,7 @@ exp_plot <- ggplot(ed_reduced, aes(x=condition,y=m,ymin=m-se,ymax=m+se,fill=cond
   geom_bar(width=1, stat="identity",position=position_dodge(width=0.4))+
   geom_errorbar(width=0.25, position=position_dodge(width=0.5),colour=rgb(0,0,0))+
   scale_fill_grey(start=0.2, end=0.6, labels=c("Four triples", "One triple"), guide=FALSE)+
-  scale_x_discrete(labels=c("1"="","2"=""))+
+  scale_x_discrete(labels=c("1"="Four triples","2"="One triple"))+
   coord_cartesian(ylim=c(0.5,1.0))+
   labs(x="\n",y="Proportion of correct responses\n", fill="Condition", title="Experiment data\n")+
   theme_bw()+
@@ -72,13 +75,13 @@ model_plot <- ggplot(pd_reduced, aes(x=model,y=m,ymin=m-se,ymax=m+se,fill=condit
   scale_fill_grey(start=0.2,end=0.6, labels=c("Four triples", "One triple"))+
   scale_x_discrete(labels=c("1"="PARSER","2"="MDLChunker","3"="TRACX"))+
   coord_cartesian(ylim=c(0.5,1.0))+
-  labs(x="\nModel",y="", fill="Condition", title="Model data\n")+
+  labs(x="\nModel",y="Probability of a correct response\n", fill="Condition", title="Model data\n")+
   theme_bw()+
   theme_bw(base_size=16,base_family="Helvetica")+
   theme(panel.grid.major.x=element_blank(), panel.grid.minor.x=element_blank(),
         panel.grid.minor.y=element_blank(), panel.grid.major.y=element_line(colour=rgb(0.4,0.4,0.4)),
         legend.position=c(0.8,0.8), panel.border=element_blank(),
-        axis.text.y=element_blank(),axis.ticks.y=element_blank(),axis.ticks.x=element_blank())
+        axis.ticks.y=element_blank(),axis.ticks.x=element_blank())
 
 grid.arrange(arrangeGrob(exp_plot,model_plot, widths=c(1/3,2/3),ncol=2))
 
